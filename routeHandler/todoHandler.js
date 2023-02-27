09635261b0c8a978545762664c26e5307865ec22
mostafa-kamal <mmkamal021@gmail.com>
2023-02-27 18:36:19 +0600

    commited

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -5,8 +5,43 @@ const todoSchema = require('../schemas/todoSchema')
 const Todo = new mongoose.model('Todo', todoSchema)
 
 // Get All The TODOS
+/*
 router.get('/', async (req, res) => {
-  //
+  await Todo.find({ status: 'active' }, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        error: 'There was a server side error!',
+      })
+    } else {
+      res.status(200).json({
+        result: data,
+        message: 'Success!',
+      })
+    }
+  })
+})
+*/
+
+router.get('/', async (req, res) => {
+  await Todo.find({ status: 'active' })
+    .select({
+      _id: 0,
+      _v: 0,
+      data: 0,
+    })
+    .limit(2)
+    .exec((err, data) => {
+      if (err) {
+        res.status(500).json({
+          error: 'There was a server side error!',
+        })
+      } else {
+        res.status(200).json({
+          result: data,
+          message: 'Success!',
+        })
+      }
+    })
 })
 
 // Get a TODO by id
@@ -46,8 +81,54 @@ router.post('/all', async (req, res) => {
 })
 
 // PUT TODO
+/*
 router.put('/:id', async (req, res) => {
-  //
+  await Todo.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: 'active',
+      },
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: 'There was a server side error!',
+        })
+      } else {
+        res.status(200).json({
+          message: 'Todo was updated successfully!',
+        })
+      }
+    }
+  )
+})
+*/
+
+router.put('/:id', async (req, res) => {
+  await Todo.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: 'active',
+      },
+    },
+    {
+      new: true,
+      useFindAndModify: false,
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: 'There was a server side error!',
+        })
+      } else {
+        res.status(200).json({
+          message: 'Todo was updated successfully!',
+        })
+      }
+    }
+  )
 })
 
 // DELETE TODO

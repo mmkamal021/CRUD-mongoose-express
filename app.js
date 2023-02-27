9b6078cfbeefa0972614af9315e4fced28f30b40
mostafa-kamal <mmkamal021@gmail.com>
2023-02-27 14:51:30 +0600

    commited

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 // express app initialization
 const app = express()
 app.use(express.json())
+
+// database connection with mongoose
+mongoose
+  .connect('mongodb://localhost/todos', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.log(err))
 
 // application routes
 
